c34343a1875774538ff42e934541a9fc65dcff28
huisheng.liu <huisheng.liu@mokun.net>
2017-12-12 18:38:30 +0800

    #2017-12-12 更新
    bloom场景修改，使高光效果更明显
    资源上传，新增一个科幻场景包，一个机器人模型

---
+++ Assets/Examples/Bloom/AutoRound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRound : MonoBehaviour {
+
+    public float _RotationSpeed = 0.1f; //定义自转的速度
+    void Start () {
+        _RotationSpeed = 0.1f;
+    }
+
+	void Update () {
+        transform.Rotate(Vector3.forward* _RotationSpeed, Space.World); //物体自转
+	}
+}
