f77c6ae098f3af273bb4ebc8972608d2d4f6f5d2
huisheng.liu <huisheng.liu@mokun.net>
2018-06-19 14:44:39 +0800

    #2018-06-19 更新
    扭曲Distort效果（基于给定的噪声图给像素加偏移）
    资源上传，新增一个特效资源包

---
+++ Assets/Examples/Distort/CreatNoiseMap.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CreatNoiseMap : MonoBehaviour {
+
+	// Use this for initialization   
+	void Start () {
+		
+	}
+
+
+    Texture2D CreatMap(int width, int height)
+    {
+        Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+
+        //遍历像素点设置  
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int tmp = Random.Range(0, 4);
+                int max = tmp * 44;
+                int r = Random.Range(0, max);
+                int g = Random.Range(0, max);
+                int b = Random.Range(0, max);
+
+                Color pixelcolor = new Color(r, g, b, 1.0f);
+                tex.SetPixel(x, y, pixelcolor);  
+            }
+        }
+
+        tex.Apply();
+
+        return tex;
+    }
+
+    void AlbumSave(Texture2D tex, System.Action<bool> callback = null)
+    {
+        System.DateTime now = new System.DateTime();
+        now = System.DateTime.Now;
+
+        byte[] bytes = tex.EncodeToPNG();
+        string filename = string.Format("NoiseMap{0}x{1}_{2}{3}{4}{5}.png", tex.width, tex.height, now.Day, now.Hour, now.Minute, now.Second);
+        string destination = Application.persistentDataPath + "";
+        if (!Directory.Exists(destination))
+        {
+            Directory.CreateDirectory(destination);
+        }
+        destination = destination + "/" + filename;
+        File.WriteAllBytes(destination, bytes);
+        if (callback != null)
+        {
+            callback(true);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {  
+            Debug.Log("Star To Creat NoiseMap ...");
+            Texture2D _tmp = CreatMap(256, 256);
+            AlbumSave(_tmp,(flag)=>
+            {
+                if(flag)
+                {
+                    Debug.Log("Creat NoiseMap Succeed !!!");
+                }
+                else
+                {
+                    Debug.LogError("Creat NoiseMap failed !!!");
+                }
+            });
+        }
+    }
+}
---
+++ Assets/Examples/Distort/Distort.shader
@@ -0,0 +1,56 @@
+﻿Shader "Custom/Distort" {
+	Properties 
+	{
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_NoiseTex ("Base (RGB)", 2D) = "black" {}
+		_MaskTex ("Mask", 2D) = "black" {}
+	}
+	
+	CGINCLUDE
+
+		#include "UnityCG.cginc"
+
+		sampler2D _MainTex;
+		sampler2D _NoiseTex;
+		uniform sampler2D _MaskTex;
+		uniform float _DistortTimeFactor;
+		uniform float _DistortStrength;
+
+		fixed4 frag(v2f_img i) : SV_Target
+		{
+			//根据时间改变采样噪声图获得随机的输出  
+			float4 noise = tex2D(_NoiseTex, i.uv - _Time.xy * _DistortTimeFactor);
+			//以随机的输出*控制系数得到偏移值  
+			float2 offset = noise.xy * _DistortStrength;
+			//像素采样时偏移offset  
+
+			//采样Mask图获得权重信息  
+			fixed4 factor = tex2D(_MaskTex, i.uv);
+			//像素采样时偏移offset，用Mask权重进行修改  
+			float2 uv = offset * factor.r + i.uv;
+
+			//float2 uv = offset + i.uv;
+			return tex2D(_MainTex, uv);
+		}
+					
+	ENDCG
+	
+	SubShader {
+		Pass
+		{
+			ZTest Always
+			Cull Off
+			ZWrite Off
+			Fog{ Mode off }
+
+			CGPROGRAM
+			#pragma vertex vert_img  
+			#pragma fragment frag  
+			//#pragma fragmentoption ARB_precision_hint_fastest   
+			#pragma fragmentoption ARB_precision_hint_nicest
+			ENDCG
+		}
+	}
+
+	FallBack Off
+}
